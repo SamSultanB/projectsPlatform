dde87889602f32d4b8486a1429657778483c9b8c
lux <ssbekmuratov@gmail.com>
2023-01-12 19:51:38 +0600

    first commit

---
+++ src/main/java/com/ProjectPlatformRest/ProjectPlatform/ProjectPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.ProjectPlatformRest.ProjectPlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectPlatformApplication.class, args);
+	}
+
+}
