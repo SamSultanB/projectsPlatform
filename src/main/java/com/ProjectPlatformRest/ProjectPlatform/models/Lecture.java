4fa3c8baba8b2309c2b72db300c2492039ef3e95
lux <ssbekmuratov@gmail.com>
2023-01-13 13:45:41 +0600

    data

--- src/main/java/com/ProjectPlatformRest/ProjectPlatform/models/Lecture.java
+++ src/main/java/com/ProjectPlatformRest/ProjectPlatform/models/Lecture.java
@@ -1,6 +1,10 @@
 package com.ProjectPlatformRest.ProjectPlatform.models;
 
+import java.util.HashSet;
+
 public class Lecture {
     private Long id;
     private String lectureName;
+    private HashSet<User> user;
+    private HashSet<Project> project;
 }
